56abaad82e564c5b5ffb14fa1545406e2dc70db6
Daniel Calissi Nakatate <daniel.nakatate@syngenta.com>
2026-04-15 19:55:25 -0300

    fix: to build the site correctly

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/log-stica-b2b-site/',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
